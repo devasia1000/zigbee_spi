827f8de30c7120bc6add2e8319a01fdef41e14d4
Devasia Manuel <devasia1000@gmail.com>
2015-11-21 19:43:43 +0000

    Finished sending Zigbee commands to turn on/off LED

--- writer.py
+++ writer.py
@@ -9,7 +9,7 @@ def init():
     spi.open(0,0)
     return spi
 
-def frame_write(spi):
+def frame_write(spi, data):
     """
     Set /SEL = L to start writing to the frame buffer
     Format is | 011 reserved[4:0] |    PHR[7:0]     |   PSDU[7:0]    | ...
@@ -17,9 +17,9 @@ def frame_write(spi):
     """
     # SRAM write starting from address 0x00
     cmd_byte = 0x60
-    length = 0x03
-   
-    print spi.xfer2([cmd_byte, length, 0xFF, 0xFF, 0xFF])
+    length = len(data)
+    
+    spi.xfer2([cmd_byte, length] + data) # append two arrays together
 
 def read_register(spi, address):
     """
@@ -37,40 +37,74 @@ def write_register(spi, address, byte):
     cmd_byte = 0xC0 | address    
     [status, empty] = spi.xfer2([cmd_byte, byte])
 
-def main(argv):
-    spi = init()
-    
+def init_chip(spi):
     # Choose channel
     write_register(spi, 0x08, 0x14)
 
     # Enable automatic FCS generation
-    write_register(spi, 0x04, 0x20)
-    
+    #write_register(spi, 0x04, 0x20)
+
+    # Disable automatic FCS generation
+    write_register(spi, 0x04, 0x00)
+
     # Enable all interrupts
     write_register(spi, 0x0e, 0xff)
 
-#    while True: 
-        
-    frame_write(spi)
-    print 'Status:', read_register(spi, 0x01)
+def set_led_on(spi):
+    print 'Going to turn LED on: '    
+
+    frame_write(spi, [0xFF, 0xFF])
+    print '\tStatus:', read_register(spi, 0x01)
 
     write_register(spi, 0x02, 0x03) # FORCE_TRX_OFF
-    print 'Status:', read_register(spi, 0x01)
+    print '\tStatus:', read_register(spi, 0x01)
 
     write_register(spi, 0x02, 0x09) # PLL_ON (TX_ON)
-    print 'Status:', read_register(spi, 0x01)
+    print '\tStatus:', read_register(spi, 0x01)
 
     write_register(spi, 0x02, 0x02) # TX_START
-    print 'Status:', read_register(spi, 0x01)           
-        
-    sleep(0.01)
-    print 'Finished sending'
-    print 'Status after send:', read_register(spi, 0x01) 
+    print '\tStatus:', read_register(spi, 0x01)
 
-    print 'IRQ status register:', read_register(spi, 0x0F), '(TRX_END should be enabled)'
-    print 'IRQ status register:', read_register(spi, 0x0F), '(TRX_END should be cleared)'
+    print '\tFinished sending'
+    print '\tStatus after send:', read_register(spi, 0x01)
 
-    print '#####################################################'
+    print '\tIRQ status register:', read_register(spi, 0x0F), '(TRX_END should be enabled)'
+    print '\tIRQ status register:', read_register(spi, 0x0F), '(TRX_END should be cleared)'
+
+    print '#####################################################'    
+
+def set_led_off(spi):
+    print 'Going to turn LED off: '
+
+    frame_write(spi, [0x01, 0x01])
+    print '\tStatus:', read_register(spi, 0x01)
+
+    write_register(spi, 0x02, 0x03) # FORCE_TRX_OFF
+    print '\tStatus:', read_register(spi, 0x01)
+
+    write_register(spi, 0x02, 0x09) # PLL_ON (TX_ON)
+    print '\tStatus:', read_register(spi, 0x01)
+
+    write_register(spi, 0x02, 0x02) # TX_START
+    print '\tStatus:', read_register(spi, 0x01)
+
+    print '\tFinished sending'
+    print '\tStatus after send:', read_register(spi, 0x01)
+
+    print '\tIRQ status register:', read_register(spi, 0x0F), '(TRX_END should be enabled)'
+    print '\tIRQ status register:', read_register(spi, 0x0F), '(TRX_END should be cleared)'
+
+    print '#####################################################'    
+
+def main(argv):
+    spi = init()
+    init_chip(spi)    
+   
+    while True: 
+        set_led_on(spi)
+        sleep(0.5)
+        set_led_off(spi)    
+        sleep(0.5)
 
 if __name__ == "__main__":
     main(sys.argv)
